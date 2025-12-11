2c5635dbcffaa91ce32b11635177f5ded736f447
Matias Enriquez <matiasenriquez97@gmail.com>
2025-12-11 11:39:49 -0300

    Add: se añadio rate limit a las rutas de autenticacion segun consigna 4

--- backend_utn-master/src/routes/authRouter.ts
+++ backend_utn-master/src/routes/authRouter.ts
@@ -1,11 +1,13 @@
 import { Router } from "express"
 import AuthController from "../controllers/authController"
+import limiter from "../middleware/rateLimitMiddleware"
 
 const authRouter = Router()
 
+//consigna 4, "Implementar rate limit únicamente en las rutas de autenticación"
 // http://localhost:3000/auth/register
-authRouter.post("/register", AuthController.register)
+authRouter.post("/register", limiter, AuthController.register)
 // http://localhost:3000/auth/login
-authRouter.post("/login", AuthController.login)
+authRouter.post("/login", limiter, AuthController.login)
 
 export default authRouter
